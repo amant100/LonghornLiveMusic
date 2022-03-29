c94eb708448cc3120918e842e6288ac74a27b93a
Rey Perez <rey.perez@utexas.edu>
2022-03-28 23:30:29 -0500

    Add feedback from to contact_us.html

--- longhorn_live_music/scripts.js
+++ longhorn_live_music/scripts.js
@@ -1,1 +1,26 @@
+var submitButton = document.getElementById("submit");
+submitButton.addEventListener("click", validateInput);
 
+var clearButton = document.getElementById("clear");
+clearButton.addEventListener("click", clearForm);
+
+function validateInput() {
+  var email = document.getElementById("email").value;
+
+  // Email validation
+  if (
+    email.match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    )
+  ) {
+    window.alert("Email is valid. Thank you for your feedback!");
+  } else {
+    window.alert("Invalid email.");
+    return;
+  }
+}
+
+function clearForm() {
+  document.getElementById("email").value = "";
+  document.getElementById("feedback").value = "";
+}
