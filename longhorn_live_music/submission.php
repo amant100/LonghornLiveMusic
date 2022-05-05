bfdb9d1b904493f0050426b98862d699b84369c8
Rey Perez <rey.perez@utexas.edu>
2022-05-04 23:04:11 -0500

    Implemented AJAX to send data and show confirmation message

--- longhorn_live_music/submission.php
+++ longhorn_live_music/submission.php
@@ -1,30 +1,23 @@
 <?php
+  error_reporting(E_ALL);
+  ini_set("display_errors", "on");
+
   $server = "spring-2022.cs.utexas.edu";
   $user = "cs329e_bulko_reyap";
   $pwd = "Farce-Idea=Duel";
   $dbName = "cs329e_bulko_reyap";
   $mysqli = new mysqli($server, $user, $pwd, $dbName);
-
   if ($mysqli->connect_errno) {
     die("Connect Error: " . $mysqli->connect_errno . ": " . $mysqli->connect_error);
   }
+  $mysqli->select_db($dbName) or die($mysqli->error);
 
-  // echo '<script type="text/javascript">alert("Your grade is 5/6");</script>';
-  // echo "<script type='text/javascript'>alert('Thank trgfdbsfgdyou for your post!');</script>";
+  $content            = $_GET["content"];
+  $contributor_name   = $_GET["contributor_name"];
+  $source             = $_GET["source"];
 
-  if (isset($_POST["content"]) and isset($_POST["name"]) and isset($_POST["source"])) {
-    $content = $_POST["content"];
-    $contributor_name = $_POST["name"];
-    $source = $_POST["source"];
-
-    $query = "INSERT INTO submissions (content, contributor_name, source) VALUES ('$content', '$contributor_name', '$source')";
-
-    // Make the messages show up with AJAX
-    if (mysqli_query($mysqli, $query)) {
-      echo "<script type='text/javascript'>alert('Thank you for your post!');</script>";
-    } else {
-      echo "Error: " . $sql . mysqli_error($mysqli);
-    }
-    mysqli_close($mysqli);
-  }
+  $query = "INSERT INTO submissions (content, contributor_name, source) VALUES ('$content', '$contributor_name', '$source')";
+  $mysqli->query($query) or die($mysqli->error);
+  echo "Thank you for your fact!";
+  mysqli_close($mysqli);
 ?>
